51eb826bcb206522b0872e636cebfbd1c7fb0ac6
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-31 16:16:35 -0500

    Recuperando ubicacion del dispositivo.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- lib/src/bloc/ubication/ubication_bloc.dart
+++ lib/src/bloc/ubication/ubication_bloc.dart
@@ -1,5 +1,9 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+
 import 'package:meta/meta.dart';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,7 +11,21 @@ part 'ubication_state.dart';
 part 'ubication_event.dart';
 
 class UbcationBloc extends Bloc<UbicationEvent, UbicationState> {
+  StreamSubscription<Position> _positionSubscription;
+
   UbcationBloc() : super(UbicationState());
+
+  void initTracing() {
+    _positionSubscription = Geolocator.getPositionStream(
+            desiredAccuracy: LocationAccuracy.high, distanceFilter: 10)
+        .listen((Position position) {
+      print(position);
+    });
+  }
+
+  void stopTracing() {
+    _positionSubscription?.cancel();
+  }
 
   @override
   Stream<UbicationState> mapEventToState(UbicationEvent event) async* {}
--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MapPage extends StatelessWidget {
+import 'package:maps_app/src/bloc/ubication/ubication_bloc.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  void initState() {
+    context.bloc<UbcationBloc>().initTracing();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    context.bloc<UbcationBloc>().stopTracing();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
